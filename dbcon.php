07e3becc32065f9cf1483fc9c4b10c301ec00c48
group20RNS <76560370+group20RNS@users.noreply.github.com>
2021-08-09 01:20:17 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,9 @@
 <?php
 	
-	$conn = mysqli_connect('localhost','root','','finalyear');
+	$conn = mysqli_connect('remotemysql.com','g6OPr2Kah8','DC7NkBzYLZ','g6OPr2Kah8');
+
+
+
 	if($conn==false)
 	{
 		echo "connection is not done";
